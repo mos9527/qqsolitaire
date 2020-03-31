20221a6d4dc5d2ce0faad85fbcc943a36e2b7d9a
greats3an <greats3an@gmail.com>
2020-03-31 22:19:15 +0800

    no info

--- qqunsolitaire.py
+++ qqunsolitaire.py
@@ -1,9 +1,9 @@
 import requests
-import parse2json
-import userio
 import json
 import re
 import logging,coloredlogs
+
+from myutils import userio,urlparams
 coloredlogs.install(logging.DEBUG)
 cookies = """
 pgv_pvid=2986671978; pgv_pvi=7280240640; RK=Ek5xPVJGUE; ptcz=ba448e2788d8d4dc443714d7d0b69cdce7a705b93715c21fc280b6036f768056; tvfe_boss_uuid=78e57206f6625467; ts_refer=ADTAGCLIENT.QQ.5603_.0; ts_uid=6431297639; eas_sid=h1D5U8f3E7H6B242P5M659D3s1; ptui_loginuin=2949306730; luin=o2949306730; lskey=00010000a0631c67cd3e5ba8eb70ffa787f9d6c38048442c5c0d49e7d69044a156d7dec8a59442e215aa5d20; pgv_si=s3977101312; _qpsvr_localtk=0.5601014086081146; uin=o2137923739; skey=@uZANbhKgr; p_uin=o2137923739; pt4_token=VpHLrHlpKTmdGG-Faf1CuRIT2Yi4aFZ7MzsA4jpI39I_; p_skey=d9WBsgaVocBCV5eGBIKIEfmrbmvxB3-UYcuqms9Kp*k_; traceid=2f5523deb0; pgv_info=ssid=s9277498560; idqq_account=theCanChange%3D1%3BtheShowUin%3D2137923739%3BBindedPhone%3D133******86%3BPhoneCanSeach%3D1%3Bmb%3D180******60%3BUinCanSeach%3D1%3Bshouldshowmail%3D1%3Bfirstsetidqq%3D1%3BMSK%3D0%3B
@@ -19,7 +19,7 @@ def _bkn(skey):
     return mask1 & mask2
 
 
-cookies = parse2json.parse2json(cookies)
+cookies =  urlparams.GetParams(cookies)
 s = requests.Session()
 s.cookies.update(cookies)
 
@@ -64,4 +64,4 @@ rawuin = getRawUin(share)
 logging.debug('真实群号：%s' % rawuin)
 
 chains = chainlist(rawuin)
-print('chain',chains)
+userio.listout(chains['data']['list'],foreach=lambda x,i:f"{x['name']}:{x['desc']}",title='接龙详情')
